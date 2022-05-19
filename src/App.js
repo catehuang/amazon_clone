8f311a4c94196aa2bca28e9adfd2efed0f79ef56
Cate Huang <s0350228@gmail.com>
2022-05-18 22:24:00 -0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+import SubHeader from './components/SubHeader';
+import Home from './components/Home';
+import Checkout from './components/Checkout';
+import Books_Home from './components/Books_Home';
+import {
+        BrowserRouter as Router,
+        Routes,
+        Route,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      {/* Home */}
-    </div>
-  );
+        return (
+                <div>
+                        <Header />
+                        <SubHeader />
+                        <Router>
+                                <div className="App">
+                                        <Routes>
+                                                <Route path="/checkout" element={<Checkout />}>
+                                                </Route>
+                                                <Route path="/books" element={<Books_Home />}>
+                                                </Route>
+                                                <Route path="/" element={<Home />}>
+                                                </Route>
+                                        </Routes>
+                                </div>
+                        </Router>
+                </div>
+        );
 }
 
 export default App;
